d5c298d1c607dd3cc13039a442889a956547c388
katsomar <katsomar60@gmail.com>
2025-08-28 17:07:52 +0300

    fixed the drop down in branch.php page

--- pages/branch.php
+++ pages/branch.php
@@ -271,6 +271,3 @@ $branch = $branch_stmt->get_result()->fetch_assoc();
 // Footer
 include '../includes/footer.php';
 ?>
-
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
