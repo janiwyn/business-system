b626e6ce5487e1e9e4d6ec9f2210cb7e0cf3b6f9
katsomar <katsomar60@gmail.com>
2025-10-17 18:53:52 +0300

    fixed the header issues

--- pages/customer_management.php
+++ pages/customer_management.php
@@ -44,7 +44,6 @@ $customers = $conn->query("SELECT * FROM customers ORDER BY id DESC");
 ?>
 
 <div class="container mt-5">
-    <h2 class="text-center mb-4">Customer Management</h2>
     <?= $message; ?>
 
     <!-- Tabs -->
